30bd8c6381be1650fa05d769d2d7314b920b547e
SABRY225 <ahmed010144497@gmail.com>
2025-05-29 23:02:51 +0300

    add

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Youtube Clone connected .');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
