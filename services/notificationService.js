37da3d3b39a21053a1b1790fe83ce978e04fe90c
SABRY225 <ahmed010144497@gmail.com>
2025-06-03 01:22:34 +0300

    second commit

--- services/notificationService.js
+++ services/notificationService.js
@@ -10,7 +10,7 @@ const notificationService = {
       if (!type || !receiverId || !message) {
         return res.status(400).json({
           success: false,
-          message: "Type and sender and message are required!",
+          message: "Type and receiver and message are required!",
         });
       }
 
